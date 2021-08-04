a2babf1d5caf6fa694f512e700142974a9881feb
Maxime Freychet <max.frecht@gmail.com>
2021-08-04 20:05:11 +0200

    [FIX] authentificator

--- migrations/Version20210804180119.php
+++ migrations/Version20210804180119.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210804173845 extends AbstractMigration
+final class Version20210804180119 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20210804173845 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD avatar VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD pseudo VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP avatar');
+        $this->addSql('ALTER TABLE user DROP pseudo');
     }
 }
